a2d1d08e7ad07cdd0b0ef8ff469b52aabfce2a28
Teluguswagger <tharunteja2u@gmail.com>
2024-02-01 20:16:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ client = tweepy.Client(
     access_token_secret,
     wait_on_rate_limit=True,
 )
-folder_path = 'bar.png'
+folder_path = '/github.com/Teluguswagger/birthdaybot/blob/main/bar.png'
 
 # Function to calculate the progress of the year
 def calculate_year_progress():
